adf8f516a4819d6f5b24bbe0b74b965b25ff9952
zhuxiaoyu1019 <rita@voiceout.pro>
2024-04-16 17:30:35 -0400

    init config localization, color, font, and input field

---
+++ voiceout/Views/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  voiceout
+//
+//  Created by Xiaoyu Zhu on 3/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

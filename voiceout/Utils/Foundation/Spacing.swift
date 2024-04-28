e5aa45f593942b7ea6ca945d0aafee66f09f4d9e
zhuxiaoyu1019 <rita@voiceout.pro>
2024-04-27 21:41:02 -0400

    add spacing

---
+++ voiceout/Utils/Foundation/Spacing.swift
@@ -0,0 +1,15 @@
+//
+//  Spacing.swift
+//  voiceout
+//
+//  Created by Xiaoyu Zhu on 4/27/24.
+//
+
+import SwiftUI
+
+extension ViewSpacing {
+    static let small: CGFloat = 8;
+    static let base: CGFloat = 12;
+    static let medium: CGFloat = 16;
+    static let large: CGFloat = 24;
+}

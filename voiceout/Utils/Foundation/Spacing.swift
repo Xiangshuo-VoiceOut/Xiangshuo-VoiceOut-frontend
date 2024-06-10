eabc31d8ab16ae5b4c2e46cb83a82d7f33101d92
JessW <jess@voiceout.pro>
2024-06-11 01:36:39 +0800

    update button and badge

--- voiceout/Utils/Foundation/Spacing.swift
+++ voiceout/Utils/Foundation/Spacing.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 extension ViewSpacing {
     static let xxsmall: CGFloat = 2;
+    static let xsmall: CGFloat = 4;
     static let small: CGFloat = 8;
     static let base: CGFloat = 12;
     static let medium: CGFloat = 16;
     static let large: CGFloat = 24;
     static let xlarge: CGFloat = 32;
     static let xxlarge: CGFloat = 57;
+    static let xxxlarge: CGFloat = 88;
+    static let xxxxlarge: CGFloat = 124;
 }

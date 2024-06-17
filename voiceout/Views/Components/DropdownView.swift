6401a7381486009d88f22920b8dfbb3722c63a4b
J.W <jess@voiceout.pro>
2024-06-17 11:25:00 -0400

    dropdown component

---
+++ voiceout/Views/Components/DropdownView.swift
@@ -0,0 +1,80 @@
+//
+//  DropdownView.swift
+//  voiceout
+//
+//  Created by J. Wu on 6/17/24.
+//
+
+import SwiftUI
+
+struct DropdownView: View {
+    let title: String
+    let prompt: String
+    let options: [String]
+    
+    @State private var isExpanded = false
+    @Binding var selection: String?
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.typography(.bodyMedium))
+                .foregroundColor(Color.textPrimary)
+                .padding(.leading, ViewSpacing.small)
+                
+            
+            VStack{
+                HStack {
+                    Text(selection ?? prompt)
+                        .foregroundColor(selection == nil ? Color.textSecondary : Color.textPrimary)
+                        .font(.typography(.bodyMedium))
+                    Spacer()
+                    Image(systemName: "chevron.down")
+                        .foregroundColor(Color.grey500)
+                        .padding(.horizontal,ViewSpacing.xsmall)
+                        .padding(.vertical, ViewSpacing.small)
+                        .rotationEffect(.degrees(isExpanded ? 180 : 0))
+                }
+                .padding(.horizontal, ViewSpacing.medium)
+                .padding(.vertical, ViewSpacing.small)
+                .onTapGesture {
+                    withAnimation(.snappy) {isExpanded.toggle()}
+                }
+                if isExpanded {
+                    VStack {
+                        ForEach(options, id: \.self) { option in
+                            HStack {
+                                Text(option)
+                                Spacer()
+                                if selection == option {
+                                    Image(systemName: "checkmark")
+                                }
+                            }
+                            .padding(ViewSpacing.baseMedium)
+                            .onTapGesture {
+                                withAnimation(.snappy) {
+                                    selection = option
+                                    isExpanded.toggle()
+                                }
+                            }
+                            
+                        }
+                    }
+                    .transition(.move(edge: .bottom))
+
+                }
+            }
+            .background(Color.surfacePrimaryGrey2)
+            .clipShape(RoundedRectangle(cornerRadius: CornerRadius.medium))
+            .shadow(radius: ViewSpacing.base)
+            .padding(ViewSpacing.small)
+            
+            
+           
+        }
+    }
+}
+
+#Preview {
+    DropdownView(title: "地区", prompt: "请选择您的地区", options: ["NY", "CA","NJ"], selection: .constant("NY"))
+}

b7f1fbd309e9493143b13977b9213d9e4da1fd29
J.W <jess@voiceout.pro>
2024-06-17 19:29:56 -0400

    addbutton-component

--- voiceout/Utils/Foundation/Border.swift
+++ voiceout/Utils/Foundation/Border.swift
@@ -10,6 +10,7 @@ import SwiftUI
 enum CornerRadius {
     case full
     case medium
+    case small
     
     var value: CGFloat{
         switch self{
@@ -17,6 +18,8 @@ enum CornerRadius {
             return 360
         case .medium:
             return 16
+        case .small:
+            return 8
         }
     }
 }
@@ -52,6 +55,8 @@ extension RoundedRectangle {
             cornerRadiusValue = 360
         case .medium:
             cornerRadiusValue = 16
+        case .small:
+            cornerRadiusValue = 8
         }
 
         self.init(cornerRadius: cornerRadiusValue, style: .continuous)

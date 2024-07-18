8ab1a8c70e0aca4cfe5ced8621cc1a50a07d8498
yujia yang <yangyujia@MacBook-Pro.local>
2024-07-18 13:59:14 -0400

    Create CardView and Reclassified files

---
+++ voiceout/Views/Common/StarRatingView（int）.swift
@@ -0,0 +1,40 @@
+//
+//  StarRatingView（int）.swift
+//  voiceout
+//
+//  Created by 阳羽佳 on 7/18/24.
+//
+
+import Foundation
+import SwiftUI
+struct StarRatingViewInt: View {
+    var rating: Int
+    let maximumRating = 5
+    let borderColor = Color(red: 0.98, green: 0.99, blue: 1)
+
+    var body: some View {
+        HStack {
+            ForEach(1...maximumRating, id: \.self) { index in
+                if index <= rating {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 3)
+                                .stroke(borderColor, lineWidth: 1)
+                        )
+                } else {
+                    Image(systemName: "star")
+                        .foregroundColor(.yellow)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 3)
+                                .stroke(borderColor, lineWidth: 1)
+                        )
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    StarRatingViewInt(rating:4)
+}

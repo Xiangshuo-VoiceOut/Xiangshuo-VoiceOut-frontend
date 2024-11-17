7211672805313b8fafe12459633bd50382c7c85e
yolandacxxx <yolanda.yy0925@gmail.com>
2024-11-16 18:30:10 -0800

    updatetherapist profile components

---
+++ voiceout/Views/Common/Profile/Components/EducationView.swift
@@ -0,0 +1,109 @@
+//
+//  EducationView.swift
+//  voiceout
+//
+//  Created by Jiaqi Chen on 10/22/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct EducationView: View {
+    var showEditButton: Bool
+    var body: some View {
+        VStack(alignment: .leading, spacing: ViewSpacing.medium) {
+            HStack(alignment: .top) {
+                Text("学历")
+                    .foregroundColor(.textBrandPrimary)
+                    .font(Font.typography(.bodyLargeEmphasis))
+                    .frame(alignment: .leading)
+                Spacer()
+                if showEditButton {
+                    EditButtonView(action: {
+                    })
+                }
+            }
+            .frame(alignment: .topLeading)
+            
+            VStack(alignment: .leading, spacing: ViewSpacing.small) {
+                VStack(alignment: .leading, spacing: ViewSpacing.xsmall) {
+                    HStack(alignment: .center) {
+                        Text("哥伦比亚大学")
+                            .foregroundColor(.textPrimary)
+                            .font(Font.typography(.bodySmall))
+                        Spacer()
+                        Text("2021年9月至2023年5月")
+                            .foregroundColor(.textSecondary)
+                            .font(Font.typography(.bodyXSmallEmphasis))
+                            .kerning(0.36)
+                            .frame(alignment: .topLeading)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .frame(alignment: .topLeading)
+                
+                HStack(alignment: .top, spacing: ViewSpacing.xsmall) {
+                    Text("硕士")
+                        .foregroundColor(.textSecondary)
+                        .font(Font.typography(.bodyXSmallEmphasis))
+                        .kerning(0.36)
+                        .frame(alignment: .topLeading)
+                    Text("|")
+                        .foregroundColor(.textSecondary)
+                    Text("心理学")
+                        .foregroundColor(.textSecondary)
+                        .font(Font.typography(.bodyXSmallEmphasis))
+                        .kerning(0.36)
+                        .frame(alignment: .topLeading)
+                }
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                
+                VStack(alignment: .leading, spacing: ViewSpacing.xsmall) {
+                    HStack(alignment: .center) {
+                        Text("纽约大学")
+                            .foregroundColor(.textPrimary)
+                            .font(Font.typography(.bodySmall))
+                        Spacer()
+                        Text("2017年9月至2021年5月")
+                            .foregroundColor(.textSecondary)
+                            .font(Font.typography(.bodyXSmallEmphasis))
+                            .kerning(0.36)
+                            .frame(alignment: .topLeading)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    
+                    HStack(alignment: .top, spacing: ViewSpacing.xsmall) {
+                        Text("学士")
+                            .foregroundColor(.textSecondary)
+                            .font(Font.typography(.bodyXSmallEmphasis))
+                            .kerning(0.36)
+                            .frame(alignment: .topLeading)
+                        Text("|")
+                            .foregroundColor(.textSecondary)
+                        Text("心理学")
+                            .foregroundColor(.textSecondary)
+                            .font(Font.typography(.bodyXSmallEmphasis))
+                            .kerning(0.36)
+                            .frame(alignment: .topLeading)
+                    }
+                    .frame(alignment: .topLeading)
+                    
+                }
+                .frame(alignment: .topLeading)
+            }
+        }
+        .padding(ViewSpacing.medium)
+        .frame(alignment: .topLeading)
+        .background(Color.surfacePrimary)
+        .cornerRadius(CornerRadius.medium.value)
+    }
+}
+
+struct EducationView_Previews: PreviewProvider {
+    static var previews: some View {
+        EducationView(showEditButton: true)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
+
